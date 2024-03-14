d702504f9c7aee83f36736b1856f0ca757b663aa
manavjangid5 <74416396+manavjangid5@users.noreply.github.com>
2024-03-14 15:00:55 +0000

    Implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+    <App/>
+    </Provider>
+  </React.StrictMode>,
 );
-reportWebVitals();
