2b653978a4045df4f8476ae31f66582bb0062146
manavjangid5 <74416396+manavjangid5@users.noreply.github.com>
2024-03-15 10:31:15 +0000

    changes made

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ function App() {
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}));
-      } else {
-        dispatch(logout());
-      }
-    })
-    .finally(() => setLoading(false));
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
   }, []);
-  
+
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
