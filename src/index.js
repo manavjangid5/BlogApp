41dc88a497a9e3bd703ef2c1af90cc5a360a3b1b
manavjangid5 <74416396+manavjangid5@users.noreply.github.com>
2024-03-15 11:21:47 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.js'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.js'
 import { AuthLayout, Login } from './components/index.js'
 
 
