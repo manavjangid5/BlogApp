d97ef7dc6daba21fd37d73f9d229d803296da1f7
Manav <manavjangid5@gmail.com>
2024-03-18 10:42:46 +0530

    chng

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png" alt="LOGO" width="50" height="50"/>
+      <img src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png" alt="LOGO" width="80" height="80"/>
     </div>
   )
 }
