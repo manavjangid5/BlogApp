ea1fc4951e618461ebc08db76d19e935bc87e326
Manav <manavjangid5@gmail.com>
2024-03-20 09:37:52 +0530

    changes

--- src/components/Header/LogoutBtn.js
+++ src/components/Header/LogoutBtn.js
@@ -2,14 +2,13 @@ import React from 'react'
 import {useDispatch} from 'react-redux'
 import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
-
 function LogoutBtn() {
 
     const dispatch = useDispatch()
-
     const logoutHandler = () => {
         authService.logout().then(() => {
             dispatch(logout())
+            window.location.reload();
         })
     }
   return (
