578a0aa499c9a1cdec0e4525cb4be0865a6f9fb9
Manav <manavjangid5@gmail.com>
2024-03-22 12:03:33 +0530

    change

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,6 +5,4 @@ const conf = {
     appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
-
 export default conf
